d28dfd96aac587802438f1423bfabfefcd60d55b
Alvinimbua <bulimua@gmail.com>
2020-10-10 00:12:44 +0300

    gads project

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,13 +1,19 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.Map;
+
 public class InfoActivity extends AppCompatActivity {
+
+    private Object Map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,7 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
@@ -25,6 +31,23 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
+
+    public void createMapIntent(View view) {
+        String geo_store = "geo:0,0?q=618 E South St Orlando, FL 32801";
+        Uri uri = Uri.parse(geo_store);
+        Intent mapIntent =new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps") ;
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+
+
+
+
+    }
+
+
     // ***
 
     // ***
